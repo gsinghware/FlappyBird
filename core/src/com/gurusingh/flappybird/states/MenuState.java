382f735c4514e1a8cda05d9757454def4ea8a422
gurusingh <gurusingh@bitbucket.org>
2015-11-25 23:25:59 -0500

    added menu screen, backgorund and a play button and used spriteback to render them to the screen

--- core/src/com/gurusingh/flappybird/states/MenuState.java
+++ core/src/com/gurusingh/flappybird/states/MenuState.java
@@ -1,29 +1,45 @@
 package com.gurusingh.flappybird.States;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.gurusingh.flappybird.FlappyBird;
 
 /**
  * Created by gsingh on 11/24/15.
  */
 public class MenuState extends State
 {
+    private Texture background;
+    private Texture playbtn;
 
-    public MenuState(GameStateManager gsm) {
+    public MenuState(GameStateManager gsm)
+    {
         super(gsm);
+        this.background = new Texture("bg.png");
+        playbtn = new Texture("playBtn.png");
     }
 
     @Override
-    public void handleInput() {
+    public void handleInput()
+    {
 
     }
 
     @Override
-    public void update(float dt) {
+    public void update(float dt)
+    {
 
     }
 
     @Override
-    public void render(SpriteBatch sb) {
+    public void render(SpriteBatch sb)
+    {
+        sb.begin();
 
+        sb.draw(this.background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+
+        sb.draw(this.playbtn, (FlappyBird.WIDTH / 2) - (playbtn.getWidth() / 2), FlappyBird.HEIGHT / 2);
+
+        sb.end();
     }
 }
